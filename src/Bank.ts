f1fc809be8ac77b50c54ad1caab35cdfe1354fa2
abhishek <abhishekspps825406@gmail.com>
2026-06-26 07:19:48 +0530

    Bank system

---
+++ src/Bank.ts
@@ -0,0 +1,46 @@
+export class BankAccount{
+    private accountNumber:number;
+    private ownerName:string;
+    private balance:number;
+
+    constructor(account:number,ownerName:string,balance:number) {
+        this.accountNumber = account;
+        this.ownerName  = ownerName;
+        this.balance = balance;
+        console.log(`Hello ${this.ownerName}! welcome onboard 🙏`)
+        console.log('====================================');
+        console.log(`Your Account number is ${this.accountNumber}`);
+        console.log('====================================');
+                
+    }
+
+    // methods
+    deposit(amount:number) {
+        if(amount < 0) {
+            console.log(`Invalid amount`)
+            return;
+        }
+        this.balance += amount;
+        console.log(`${amount} has been successfully deposited in your account`)
+    }
+
+    withdraw(amount:number) {
+        if(amount > this.balance) {
+            console.log(`Insufficient balance`);
+        }else {
+            this.balance -= amount;
+            console.log(`${amount} amount has been withdrawn from your account`);
+        }
+    }
+
+    checkBalance() {
+        console.log(`Your current balance is ${this.balance}`)
+    }
+
+}
+
+
+const Bank1 = new BankAccount(777,"Raj Shah",0)
+Bank1.checkBalance()
+Bank1.deposit(43)
+Bank1.withdraw(22)
