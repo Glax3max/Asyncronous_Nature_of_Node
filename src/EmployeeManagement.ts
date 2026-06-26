b094eec9315d8197821acd476684763f5645a941
abhishek <abhishekspps825406@gmail.com>
2026-06-26 07:58:51 +0530

    Employee management done

---
+++ src/EmployeeManagement.ts
@@ -0,0 +1,39 @@
+export class Employee{
+    // properies
+    private readonly id:string;
+    private name:string;
+    private salary:number;
+
+    constructor(name,id,salary){
+        this.id = id;
+        this.salary = salary;
+        this.name = name;
+        console.log('====================================');
+        console.log(`Welcome onboard on HRM ${name}`);
+        console.log('====================================');
+    }
+    
+    // Methods
+    increaseSalary(percent:number) {
+        if(percent < 0) {
+            console.log('====================================');
+            console.log("Enter a valid percent value");
+            console.log('====================================');
+            return;
+        }
+        this.salary  += (percent/100)*this.salary;
+        console.log('====================================');
+        console.log(`Congratulations 🥳🥳 your new salary is ${this.salary}`);
+        console.log('====================================');
+    }
+
+    dispalyDetails() {
+        console.log('====================================');
+        console.log(`Employee id:${this.id}`);
+        console.log(`Employee Name:${this.name}`);
+        console.log(`Employee Salary:${this.salary}`);
+        console.log('====================================');        
+    }
+
+
+}
