505f04badca481bb3befc408d9c39c78763301bc
abhishek <abhishekspps825406@gmail.com>
2026-06-26 07:02:29 +0530

    Testing2

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,25 @@
-// custome promise.all implementation 
-async function customePromiseAll5<T>(promises:(Promise<T> | T)[]):Promise<T[]> {
-    return new Promise((resolve,reject)=> {
-      const results:T[] = [];
-      let completedPromise = 0;
+export class Adding{
+    private sum:number = 0;
+    private num1:number;
+    private num2:number;
+    constructor(num1:number,num2:number){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
 
-      promises.forEach((promise,index)=> {
-        Promise.resolve(promise).then((data)=> {
-          results[index] = data;
-          completedPromise++;
+    calculateSum():number {
+        return this.num1+this.num2;
+    }
 
-          if(completedPromise == promises.length) {
-            resolve(results);
-          }
-        })
-        .catch((err)=> {
-          reject(err);
-        });
-      });
-
-      if(promises.length == 0) resolve([])
-    });
+    healthCheck():Object {
+        return {
+            status:"Hello EveryOne"
+        }
+    }
 }
-const promise1 = async ()=>({id:1,value:"Promise1"})
-const promise2 = async ()=>({id:2,value:"Promise2"})
-const promise3 = async ()=>({id:3,value:"Promise3"})
 
-customePromiseAll5([promise1(),promise2(),promise3()]).then((value)=> {
-  value.forEach((data:any,index)=> {
-    console.log(data.value,"  -> ",index);
-  })
-})
+
+const S1 = new Adding(34,23);
+
+console.log(S1.healthCheck())
+console.log(S1.calculateSum())
