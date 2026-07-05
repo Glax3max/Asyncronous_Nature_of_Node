fdafa5985b6165018dac48fa11ea40adbcd76de4
abhishek <abhishekspps825406@gmail.com>
2026-07-05 13:25:32 +0530

    Improving the design pattern

--- src/PaymentSystem.ts
+++ src/PaymentSystem.ts
@@ -1,6 +1,4 @@
 interface PaymentGateway {
-    pay(amount:number):void,
-    refund():void,
     getTransactionDetails():string
 }
 enum PaymentStatus{
@@ -51,6 +49,20 @@ abstract class Payment{
         console.log(`Status:${this.payment.status}`);
     }
 
+
+    /**
+     * Making the refund
+     */
+    refund():void {
+        if(this.payment.status !== PaymentStatus.Done) 
+            throw new Error("Refund can't be initiated") 
+        this.validatePayment()
+        this.payment.status = PaymentStatus.Refunded;
+        this.payment.date = new Date();
+        console.log("Payment refunded")
+    }
+
+
     /**
      * 
      * Payment Operations
@@ -63,11 +75,24 @@ abstract class Payment{
         this.payment.date =new Date();
     }
 
+     /**
+     * Getting the transaction detail
+     */
+    getTransactionDetails():string {
+        if(this.payment.status === PaymentStatus.NotInitialted) {
+        throw new Error("Payment not initiated");
+            }
+        return `Transaction done and this time it is a ${this.payment.status} operation`;
+        }
     /**
      * validate payment abstract method
      */
     abstract validatePayment():void;
 
+    /**
+     * Print transactional detail
+     */
+    abstract printTransactionDetail():void;
 }
 
 class UpiPayment 
@@ -79,16 +104,6 @@ class UpiPayment
         protected readonly bankName:string
     ) {
         super(payment)
-    }
-
-    /**
-     * Making the refund
-     */
-    refund():void {
-        if(this.payment.status !== PaymentStatus.Done) 
-            throw new Error("Refund can't be initiated") 
-        this.payment.status = PaymentStatus.Refunded;
-        console.log("Refund successfully done")
     }
 
     /**
@@ -116,16 +131,6 @@ class UpiPayment
         console.log(`Upi Id : ${this.upiId}`);
         console.log(`Bank Name : ${this.bankName}`);
     }
-
-    /**
-     * get transaction detail
-     */
-    getTransactionDetails():string {
-        if(this.payment.status == PaymentStatus.NotInitialted) {
-            throw new Error(`Status is not initialed`)
-        }
-        return `Upi Id: ${this.upiId} is used as the transaction destination and the amount is ${this.payment.amount}`
-    }
 }
 
 class CreditCardPayment
@@ -133,7 +138,7 @@ class CreditCardPayment
     implements PaymentGateway {
         constructor(
             payment:PaymentDetail,
-            private readonly cardNumber:number,
+            private readonly cardNumber:string,
             private readonly bankName:string,
             private readonly cvv:number,
             private readonly pin:number,
@@ -142,19 +147,8 @@ class CreditCardPayment
         }
 
         /**
-         * Refunding the money
-         */
-        refund():void{
-            this.validatePayment()
-            this.payment.status = PaymentStatus.Refunded;
-            this.payment.date = new Date();
-            console.log("Payment refunded")
-        }
-
-        /**
          * Validating the payment
          */
-
         validatePayment():void {
             if(this.payment.amount <= 0 )
                 throw new Error("Amount should be greater than 0")
@@ -181,15 +175,7 @@ class CreditCardPayment
         console.log(`Card Number : ${this.cardNumber}`);
         console.log(`Bank Name : ${this.bankName}`);
         }
-        /**
-         * Getting the transaction detail
-         */
-        getTransactionDetails():string {
-            if(this.payment.status === PaymentStatus.NotInitialted) {
-                throw new Error("Payment not initiated");
-            }
-            return `Transaction done and this time it is a ${this.payment.status} operation`;
-        }
+       
     }
 
 const intialPaymentData:PaymentDetail ={
@@ -200,7 +186,7 @@ const intialPaymentData:PaymentDetail ={
 }
 
 const upiPayment1 = new UpiPayment(intialPaymentData,"328748@23487","BOI")
-const cardPayment = new CreditCardPayment(intialPaymentData,890764527628,"SBI",874,349294)
+const cardPayment = new CreditCardPayment(intialPaymentData,"890764527628","SBI",874,349294)
 
 cardPayment.pay(348743);
 cardPayment.refund();
