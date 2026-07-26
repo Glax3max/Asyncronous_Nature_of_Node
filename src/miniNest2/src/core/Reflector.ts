9d87e4e593a121b74c512f61067386dc97c9390d
abhishek <abhishekspps825406@gmail.com>
2026-07-26 10:14:35 +0530

    Reflector done

--- src/miniNest2/src/core/Reflector.ts
+++ src/miniNest2/src/core/Reflector.ts
@@ -1,19 +1,67 @@
+import { MetadataKeys } from './metadata-key';
+import "reflect-metadata"
 
+export type MetadataKey =
+    (typeof MetadataKeys)[keyof typeof MetadataKeys];
 
-class Reflector {
+export class Reflector {
     
     /**
      * Defines the meta data of a decorator attached function or class
      */
-    defineMetadata(){}
+    public defineMetadata(
+        key:MetadataKey,
+        value:unknown,
+        target:object,
+        propertyKey?:string|symbol,
+    ):void{
+        if (propertyKey !== undefined) {
+         Reflect.defineMetadata(key, value, target, propertyKey);
+        } else {
+         Reflect.defineMetadata(key, value, target);
+        }
+    }
 
     /**
      * Based on the demand the DI is done with this
      */
-    getMetadata(){}
+    public getMetadata<T>(
+        key:MetadataKey,
+        target:object,
+        propertyKey?:string|symbol
+    ):T|undefined {
+        if(propertyKey !== undefined) {
+            return Reflect.getMetadata(
+                key,
+                target,
+                propertyKey
+            )as T | undefined;
+        }
+        
+        return Reflect.getMetadata(
+            key,
+            target
+        )as T | undefined;
+    }
 
     /**
      * Check if an entry across some key exist or not
      */
-    hasMetadata(){}
+    hasMetadata(
+        key:MetadataKey,
+        target:object,
+        propertyKey?:string|symbol
+    ):boolean{
+        if(propertyKey !== undefined) {
+            return Reflect.hasMetadata(
+                key,
+                target,
+                propertyKey
+            )
+        }
+        return Reflect.hasMetadata(
+            key,
+            target
+        )
+    }
 }
