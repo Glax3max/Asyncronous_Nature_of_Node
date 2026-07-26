9cc96d0cb3e333bc03f297726c5fdf9cd0172034
abhishek <abhishekspps825406@gmail.com>
2026-07-26 08:36:05 +0530

    Making the reflector

---
+++ src/miniNest2/src/core/Reflector.ts
@@ -0,0 +1,19 @@
+
+
+class Reflector {
+    
+    /**
+     * Defines the meta data of a decorator attached function or class
+     */
+    defineMetadata(){}
+
+    /**
+     * Based on the demand the DI is done with this
+     */
+    getMetadata(){}
+
+    /**
+     * Check if an entry across some key exist or not
+     */
+    hasMetadata(){}
+}
--- src/miniNest2/src/core/metadata-scanner.ts
+++ src/miniNest2/src/core/metadata-scanner.ts
@@ -1,3 +1,4 @@
 /**
  * Discovers
  */
+
